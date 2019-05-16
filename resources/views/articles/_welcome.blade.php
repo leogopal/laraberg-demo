97b2ead24379362057ff4f90a5efd110eebc8c68
Maurice Wijnia <mauricewijnia@live.nl>
2019-05-16 11:58:39 +0200

    Mobile fixes

--- resources/views/articles/_welcome.blade.php
+++ resources/views/articles/_welcome.blade.php
@@ -1,4 +1,5 @@
 <div class="top-content">
+    <div class="top-content-background-overlay"></div>
     <div class="top-content-background" uk-parallax="bgy: -50; easing: -1;"></div>
     <div class="top-content-container">
         <div class="uk-container uk-container-small">
@@ -11,8 +12,10 @@
                     Please report any issues you find to the Laraberg or the Laraberg Demo Github project.
                 </p>
             </div>
-            <a href="https://github.com/VanOns/laraberg" target="_blank" class="uk-button uk-button-default uk-light"><span uk-icon="github" class="uk-margin-right"></span>Laraberg</a>
-            <a href="https://github.com/VanOns/laraberg-demo" target="_blank" class="uk-button uk-button-default uk-light"><span uk-icon="github" class="uk-margin-right"></span>Laraberg-Demo</a>
+            <div uk-margin>
+                <a href="https://github.com/VanOns/laraberg" target="_blank" class="uk-button uk-button-default uk-light"><span uk-icon="github" class="uk-margin-right"></span>Laraberg</a>
+                <a href="https://github.com/VanOns/laraberg-demo" target="_blank" class="uk-button uk-button-default uk-light"><span uk-icon="github" class="uk-margin-right"></span>Laraberg-Demo</a>
+            </div>
         </div>
     </div>
     <svg class="svg-overlay" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="isolation:isolate" viewBox="0 0 560 560" width="560" height="560"><defs><clipPath id="_clipPath_VjAhMJAHMKXMJoTPznRM8Z6W5qCqy3MQ"><rect width="560" height="560"/></clipPath></defs><g clip-path="url(#_clipPath_VjAhMJAHMKXMJoTPznRM8Z6W5qCqy3MQ)"><path d=" M -3.007 515.932 L 434.479 547.932 L 573 504 L 572.003 568 L -5 564.203 L -3.007 515.932 Z " fill="rgb(255,255,255)"/></g></svg>
